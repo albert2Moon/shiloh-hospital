0a56e8849e54512841cc76d58102a35a47c1c7d7
Albert Watbin <albertwatbin@gmail.com>
2024-08-22 18:32:46 +0300

    npm run build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['/src/main.jsx']
-    }
+    outDir: 'build' // Change 'build' to your desired output directory
   }
 })

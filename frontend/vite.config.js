21c73d2deb3356978923c6575cd2c57c7dda2d7b
Albert Watbin <albertwatbin@gmail.com>
2024-08-22 23:20:25 +0300

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build' // Change 'build' to your desired output directory
+    outDir: 'build', // Change 'build' to your desired output directory
+    rollupOptions: {
+      external: []  // Ensure this does not include `/src/main.jsx`
+    }
   }
 })
